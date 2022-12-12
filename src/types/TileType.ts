bb60bc4b094957f54570cc5c4cec5c120186c6d7
beatlz <adrian@basave.com>
2022-12-12 22:55:02 +0100

    feat(types): create TileType

---
+++ src/types/TileType.ts
@@ -0,0 +1,29 @@
+export enum tileColor {
+  LIGHT,
+  DARK
+}
+
+export enum featuresList {
+  TELEPORT,
+  FREEZE,
+  LADDER,
+  SPAWNER,
+  ARMOR,
+  ADD_MOVE
+}
+
+export type TileColorType = keyof typeof tileColor
+
+export type FeatureNameType = keyof typeof featuresList
+
+export interface TileFeatureType {
+  name: FeatureNameType
+  action: (params: any) => any // To do: rework
+}
+
+export type TileFeaturesListType = TileFeatureType[]
+
+export interface TileType {
+  isActive: boolean
+  features: TileFeaturesListType 
+}
