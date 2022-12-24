81329e38b28e94647d49b0c389a89d180d9d6b48
Adrián <85233867+adrianBasave@users.noreply.github.com>
2022-12-24 16:35:01 +0100

    update(name types): add move, item, buff, debuff

--- src/types/PieceType.ts
+++ src/types/PieceType.ts
@@ -13,14 +13,37 @@ export enum moveNames {
   // Register here all move names that are not 
   // default for a piece.
   CHANCELLOR,
-  ARCBISHOP
+  ARCHBISHOP
+}
+
+export enum itemNames {
+  TELEPORTER,
+  FREEZEGUN,
+  COSTUME,
+  LASSO,
+}
+
+export enum buffNames {
+  HUNGER,
+}
+
+export enum debuffNames {
+  SLEEP,
+  STUN,
+  ROOT,
 }
 
 export type PieceNameType = keyof typeof pieceNames
 
 export type MoveNameType = keyof typeof moveNames | PieceNameType
 
-export type MoveType = (CoordsType | MoveNameType)[]
+export type itemNameType = keyof typeof itemNames
+
+export type buffNameType = keyof typeof buffNames
+
+export type debuffNameType = keyof typeof debuffNames
+
+export type MoveType = CoordsType[]
 
 export type GetMovesType = (initialSquare: CoordsType) => MoveType
 
@@ -31,9 +54,9 @@ export interface PieceInitOptionsType {
 
 export interface PieceDescriptionType {
   lives: number
-  moves: (GetMovesType | string)[]
+  moves: GetMovesType[]
   inventory?: ItemType[]
   buffs?: BuffType[]
 }
 
-export type PieceType = PieceDescriptionType | PieceNameType
+export type PieceType = PieceDescriptionType
