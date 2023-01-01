d865db59e424e8f7431e8683c370fcf03a22b184
Adrián <85233867+adrianBasave@users.noreply.github.com>
2023-01-01 12:54:06 +0100

    update: add diagonal generic move

--- src/modules/Piece/Move/MoveFunctions/BishopMove.ts
+++ src/modules/Piece/Move/MoveFunctions/BishopMove.ts
@@ -1,26 +1,9 @@
 import type CoordsType from "@/types/CoordsType"
 import type { GetMovesType } from "@/types/PieceType"
+import { diagonalMove } from "./GenericMoves"
 
 const bishopMove: GetMovesType = (initCoords: CoordsType, layoutSize: number): CoordsType[] => {
-	const c = initCoords.x - initCoords.y
-	const max = layoutSize - 1
-	const legalMoves = []
-
-	for (let x = 0; x <= max; x++) {
-		let y = x + c
-
-		if (x < max && y < max) {
-			legalMoves.push({ x, y })
-		}
-
-		y = x * -1 + c
-
-		if (x < max && y >= 0) {
-			legalMoves.push({ x, y })
-		}
-	}
-
-	return legalMoves
+	return diagonalMove(initCoords, layoutSize)
 }
 
 export default bishopMove
--- src/modules/Piece/Move/MoveFunctions/GenericMoves.ts
+++ src/modules/Piece/Move/MoveFunctions/GenericMoves.ts
@@ -24,3 +24,25 @@ export const verticalMove: GetMovesType = (
 
 	return legalMoves
 }
+
+export const diagonalMove: GetMovesType = (initCoords: CoordsType, layoutSize: number): CoordsType[] => {
+	const c = initCoords.x - initCoords.y
+	const max = layoutSize - 1
+	const legalMoves = []
+
+	for (let x = 0; x <= max; x++) {
+		let y = x + c
+
+		if (x < max && y < max) {
+			legalMoves.push({ x, y })
+		}
+
+		y = x * -1 + c
+
+		if (x < max && y >= 0) {
+			legalMoves.push({ x, y })
+		}
+	}
+
+	return legalMoves
+}
