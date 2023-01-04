a3ab5fd555234a19d12cc2234cf4cfa08a638e94
Adrián <85233867+adrianBasave@users.noreply.github.com>
2023-01-04 17:32:54 +0100

    update(re-export): add index.ts file

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,31 @@
-require(`module-alias/register`)
+/* TYPES */
+export * from "./types/constants"
+export * from "./types/CoordsType"
+export * from "./types/DungeonType"
+export * from "./types/PieceType"
+export * from "./types/RulesType"
+/* MODULES */
+// // Dungeon
+export * from "./modules/Dungeon/Dungeon"
+// // // Tile
+export * from "./modules/Dungeon/Tile/Tile"
+// // Game
+// // // Rules
+export * from "./modules/Game/Rules/Rules"
+// // Piece
+export * from "./modules/Piece/Piece"
+// // // Moves
+export * from "./modules/Piece/Moves/GenericMoves"
+export * from "./modules/Piece/Moves/BishopMove"
+export * from "./modules/Piece/Moves/RookMove"
+export * from "./modules/Piece/Moves/KnightMove"
+export * from "./modules/Piece/Moves/QueenMove"
+export * from "./modules/Piece/Moves/KingMove"
+export * from "./modules/Piece/Moves/PawnMove"
+// // // Presets
+export * from "./modules/Piece/Presets/rook"
+export * from "./modules/Piece/Presets/knight"
+export * from "./modules/Piece/Presets/bishop"
+export * from "./modules/Piece/Presets/queen"
+export * from "./modules/Piece/Presets/king"
+export * from "./modules/Piece/Presets/pawn"
