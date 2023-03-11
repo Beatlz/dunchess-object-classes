309bafcd5a18ac16bd572d9ed8bd85bd37b7304b
beatlz <adrian@basave.com>
2023-03-11 12:00:50 +0100

    update(exports): add utility, square, serializer

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,9 @@ export * from "./types/RulesTypes"
 /* MODULES */
 // // Dungeon
 export * from "./modules/Dungeon/Dungeon"
+export * from "./modules/Dungeon/serializer"
+// // // Square
+export * from "./modules/Dungeon/Square/Square"
 // // // Tile
 export * from "./modules/Dungeon/Tile/Tile"
 // // Game
@@ -27,3 +30,7 @@ export * from "./modules/Piece/Moves/PawnMove"
 // // // Presets
 export * from "./modules/Piece/Presets/PiecePresetList"
 export * from "./modules/Piece/Presets/Preset"
+/* UTILITY */
+export * from "./utility/filterMap"
+export * from "./utility/isEven"
+export * from "./utility/serializeKey"
