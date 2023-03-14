d20694c7260d02634ee0957e61ad530157eab394
beatlz <adrian@basave.com>
2023-03-14 15:05:22 +0100

    feat(serializer): add deserializeKey

--- src/utility/serializeKey.ts
+++ src/utility/serializeKey.ts
@@ -3,3 +3,17 @@ import { CoordsType } from "../types/CoordsType"
 export const serializeKey = ({ x, y }: CoordsType): string => {
 	return `x${x}y${y}`
 }
+
+export const deserializeKey = (serializedKey: string): CoordsType | null => {
+	const regex = /x(\d+)y(\d+)/
+	const match = serializedKey.match(regex)
+
+	if (match) {
+		const x = parseInt(match[1], 10)
+		const y = parseInt(match[2], 10)
+		
+		return { x, y }
+	}
+
+	return null
+}
