5decd6ac8c82c1300aa06b429d78de95080c2f6f
beatlz <adrian@basave.com>
2023-03-18 01:59:59 +0100

    update(Dungeon): add cropLayout and changeLayoutSize

--- src/modules/Dungeon/Dungeon.ts
+++ src/modules/Dungeon/Dungeon.ts
@@ -5,7 +5,7 @@ import type {
 } from "../../types/DungeonTypes"
 import { DUNGEON_SIZE } from "../../types/constants"
 import { Square } from "./Square/Square"
-import { serializeKey } from "../../utility/serializeKey"
+import { deserializeKey, serializeKey } from "../../utility/serializeKey"
 import { createLayout } from "./createLayout"
 
 export class Dungeon {
@@ -32,6 +32,58 @@ export class Dungeon {
 		return as2D === `2D`
 			? totalSquares
 			: Math.sqrt(totalSquares)
+	}
+	changeLayoutSize(size: number): void {
+		const layout = createLayout(size)
+		const layoutSize = this.layoutSize()
+
+		if (size > layoutSize) {
+			this.layout = {
+				...layout,
+				...this.layout,
+			}
+
+			return
+		}
+
+		Object.keys(this.layout).forEach((key) => {
+			const square = this.layout[key]
+			const { x, y, piece, tile } = square.description
+
+			if ((piece || tile) && x < layoutSize && y < layoutSize) {
+				layout[key] = square
+			}
+		})
+	}
+	cropLayout() {
+		const coords = Object.keys(this.layout).map(key => deserializeKey(key))
+		const X = coords.map(coord => coord?.x) as number[]
+		const Y = coords.map(coord => coord?.x) as number[]
+		const xMin = Math.min(...X)
+		const xMax = Math.max(...X)
+		const yMin = Math.min(...Y)
+		const yMax = Math.max(...Y)
+		const isXSmaller = xMax < this.layoutSize()
+		const isYSmaller = yMax < this.layoutSize()
+		// Layout size is not croppable
+		// at least one dimension needs all space
+		if (!isXSmaller && !isYSmaller) return
+		
+		const newSize = Math.max(xMax - xMin, yMax - yMin)
+		const layout = createLayout(newSize)
+
+		for (const coord of coords) {
+			if (coord) {
+				const { x, y } = coord
+				const { color, isActive } = this.getSquare({ x, y }).description
+				const newX = x - xMin
+				const newY = y - yMin
+				
+				this.setSquare({ color, isActive, x: newX, y: newY })
+			}
+		}
+	
+		this.layout = layout
 	}
 
 	getSquare({ x, y }: CoordsType): Square {
